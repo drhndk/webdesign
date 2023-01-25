2002ffc27e65652e2255625b838dea6efa9a3fcd
drhndk <dhurhankusnaidi@gmail.com>
2023-01-26 01:43:33 +0700

    web design commit

---
+++ script.js
@@ -0,0 +1,14 @@
+const btnStarted = document.querySelector(".btn");
+const slideStarted = document.querySelector(".content");
+
+btnStarted.addEventListener("click", () => {
+  slideStarted.classList.toggle("main");
+});
+
+const faSharp = document.querySelector(".fa-sharp");
+btnStarted.addEventListener("mouseenter", () => {
+  faSharp.style.backgroundColor = "aqua";
+});
+btnStarted.addEventListener("mouseleave", () => {
+  faSharp.style.backgroundColor = "rgba(138, 27, 27, 0.521)";
+});
